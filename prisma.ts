a7770acefd9291ca7439cc7e8006d4a68b0ea179
juannMGC <jugarciaci@unal.edu.co>
2025-06-19 12:32:32 -0500

    Ejecutar una consulta de búsqueda simple con Prisma

--- prisma.ts
+++ prisma.ts
@@ -1,5 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 async function runPrisma() {
-    console.log("Hello Prisma...");
+  // find all posts
+  const allPosts = await prisma.post.findMany();
+  console.log(allPosts);
+
+  // find posts with authors
+  const allPostsWithAuthors = await prisma.post.findMany({
+    include: {
+      author: true,
+    },
+  });
+  console.log(allPostsWithAuthors);
+
+  // find posts with authors and categories
+  const allPostsWithAuthorsAndCategories = await prisma.post.findMany({
+    include: {
+      author: true,
+      categories: true,
+    },
+  });
+  console.log(allPostsWithAuthorsAndCategories);
+
+  // create a post
+  const newPost = await prisma.post.create({
+    data: {
+      title: "My new post",
+      content: "My new post content",
+      author: {
+        connect: {
+          id: "some author id from your database",
+        },
+      },
+    },
+  });
+  console.log(newPost);
+
+  // update a post
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: "some post id from your database",
+    },
+    data: {
+      content: "My new post content updated",
+    },
+  });
+  console.log(updatedPost);
+
+  // delete a post
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id: "some post id from your database",
+    },
+  });
+  console.log(deletedPost);
 }
 
 runPrisma();
